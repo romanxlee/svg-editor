9e1715f68e4c17667ed229033c62d773c25faa73
romanxlee <romanxlee@yandex.ru>
2021-06-03 18:22:47 +0300

    add draging of circle

--- src/components/VCanvas.vue
+++ src/components/VCanvas.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <canvas :id="canvasId" class="canvas-style" v-on:mousemove="onMouseMove"
+        <canvas :id="canvasId" class="canvas-style" v-on:mousedown="mouseMove"
         />
     </div>
 </template>
@@ -11,32 +11,41 @@
         props: ['canvasId'],
         data: () => ({
             path: null,
-            scope: null
+            scope: null,
+            newCircle: null
         }),
         methods: {
             circleCreate(scope) {
                 scope.activate();
                 const myCircle = new paper.Path.Circle(new paper.Point(100, 70), 50);
                 myCircle.fillColor = 'black'
-                return myCircle
+                return this.newCircle =  myCircle
             },
-            rectangleCreate(scope) {
+            /* rectangleCreate(scope) {
               scope.activate();
               const myRectangle = new paper.Rectangle(new paper.Point(450, 350), new paper.Point(150, 100))
               const myPath = new paper.Path.Rectangle(myRectangle)
               myPath.fillColor = 'black'
-              myPath.selected = true
               return myPath
+            }, */
+            createTool(scope) {
+              scope.activate();
+              return new paper.Tool();
             },
-            onMouseMove(event) {
-              this.myCircle.position = event.point;
+            mouseMove() {
+              let self = this;
+              this.tool = this.createTool(this.scope);
+              this.tool.onMouseDrag = (event) => {
+                self.path = this.newCircle
+                self.path.position = event.point
+              }
             }
         },
         mounted() {
             this.scope = new paper.PaperScope();
             this.scope.setup(this.canvasId);
             this.circleCreate(this.scope)
-            this.rectangleCreate(this.scope)
+            //this.rectangleCreate(this.scope)
         }
     }
 </script>
