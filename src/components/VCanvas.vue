8dcfd3487768b8a21e9bcc0782a67020437d2023
romanxlee <romanxlee@yandex.ru>
2021-06-04 19:35:34 +0300

    add some styles

--- src/components/VCanvas.vue
+++ src/components/VCanvas.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        <canvas :id="canvasId" class="canvas-style">
-        </canvas>
+  <div class="container">
+    <div class="buttons-container">
         <div class="button" @click="circleCreate">ADD CIRCLE</div>
         <div class="button" @click="rectangleCreate">ADD RECTANGLE</div>
         <div class="button" @click="triangleCreate">ADD TRIANGLE</div>
         <div class="button" @click="starCreate">ADD STAR</div>
-        <ul v-for="item in figures" :key="item.id" :item="item">
-          <li>{{item.id}}<button @click="removeFigure(item, item.id)"></button></li>
-        </ul>
     </div>
+
+    <canvas :id="canvasId" class="canvas-style">
+    </canvas>
+
+    <div class="log">
+      <div class="log__item" v-for="item in figures" :key="item.id">
+        <p class="log__text">
+          ID: {{item.id}},
+        </p>
+        <p class="log__text">
+          Создан в: {{item.date}},
+        </p>
+        <p class="log__text">
+          Тип: {{item.description}}
+        </p>
+        <div class="button button_remove" @click="removeFigure(item, item.id)"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,14 +44,8 @@
                 myCircle.fillColor = 'black'
                 myCircle.onMouseDrag = (event) => this.mouseDrag(myCircle, event)
                 myCircle.onMouseUp = () => this.mouseUp(myCircle)
-                /* myCircle.onMouseDrag = (event) => {
-                myCircle.selected = true
-                myCircle.position = event.point
-              } */
-                /* myCircle.onMouseUp = () => {
-                  myCircle.selected = false
-                } */
-                myCircle.date = new Date()
+                myCircle.description = 'Круг'
+                myCircle.date = new Date().toLocaleString('ru', {hour: 'numeric', minute: 'numeric', second: 'numeric'})
                 this.figures.push(myCircle)
                 return myCircle
             },
@@ -46,6 +55,8 @@
               myPath.fillColor = 'blue'
               myPath.onMouseDrag = (event) => this.mouseDrag(myPath, event)
               myPath.onMouseUp = () => this.mouseUp(myPath)
+              myPath.description = 'Прямоугольник'
+              myPath.date = new Date().toLocaleString('ru', {hour: 'numeric', minute: 'numeric', second: 'numeric'})
               this.figures.push(myPath)
               return myPath
             },
@@ -54,6 +65,8 @@
               myTriangle.fillColor = 'yellow'
               myTriangle.onMouseDrag = (event) => this.mouseDrag(myTriangle, event)
               myTriangle.onMouseUp = () => this.mouseUp(myTriangle)
+              myTriangle.description = 'Треугольник'
+              myTriangle.date = new Date().toLocaleString('ru', {hour: 'numeric', minute: 'numeric', second: 'numeric'})
               this.figures.push(myTriangle)
               return myTriangle
             },
@@ -68,6 +81,8 @@
               newStar.rotate(35)
               newStar.onMouseDrag = (event) => this.mouseDrag(newStar, event)
               newStar.onMouseUp = () => this.mouseUp(newStar)
+              newStar.description = 'Звезда'
+              newStar.date = new Date().toLocaleString('ru', {hour: 'numeric', minute: 'numeric', second: 'numeric'})
               this.figures.push(newStar)
               return newStar
             },
@@ -104,18 +119,50 @@
 </script>
 
 <style scoped>
+.container {
+  width: 100%;
+  display: flex;
+  gap: 10px;
+}
     .canvas-style {
         cursor: pointer;
-        width: 100% !important;
+        width: 80% !important;
         height: 500px !important;
         border: 5px solid black;
         border-radius: 10px;
         display: block;
-        margin: auto;
-        box-shadow: 0 10px 8px -8px black;
     }
     .button {
       border: 1px solid black;
       cursor: pointer;
+    }
+
+    .button_remove {
+      background-image: url(../assets/delete.svg);
+      background-size: cover;
+      background-repeat: no-repeat;
+      width: 15px;
+      height: 15px;
+      border: none;
+    }
+
+    .log {
+      width: 15%;
+      max-height: 500px;
+      border: 1px solid black;
+      overflow-y: auto;
+    }
+
+    .log__item {
+      display: flex;
+      flex-direction: column;
+      gap: 5px;
+      align-items: center;
+      padding: 5px 0;
+      outline: 1px solid black;
+    }
+
+    .log__text {
+      margin: 0;
     }
 </style>
